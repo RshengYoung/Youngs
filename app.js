f3afab8d53d1e0b932eb4dcc048d53542fc186e4
RSHENG\Rsheng <yunsun60110@gmail.com>
2017-12-26 20:47:20 +0800

    Set environment variable

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log("%s listening to %s", server.name, server.url);
 });
-let connector = new builder.ChatConnector();
+let connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
+});
 server.post("/api/messages", connector.listen());
 let bot = new builder.UniversalBot(connector, session => {
     session.send("You said: %s....", session.message.text);
